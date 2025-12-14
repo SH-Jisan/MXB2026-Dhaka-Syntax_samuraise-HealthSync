740e7b26635911718ffd0c6944f5e124551dd36f
SH <shzisun123@gmail.com>
2025-12-14 22:20:03 +0600

    folder structure

---
+++ health_sync/lib/core/constants/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF00796B); // Medical Teal
+  static const secondary = Color(0xFF004D40);
+  static const accent = Color(0xFF00BFA5);
+
+  static const background = Color(0xFFF8FAFC);
+  static const surface = Colors.white;
+  static const error = Color(0xFFEF4444);
+
+  static const textPrimary = Color(0xFF1E293B);
+  static const textSecondary = Color(0xFF64748B);
+}
